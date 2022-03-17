649c8947f75245a7df03f957b64c2041d2f90a9f
thegux <gabrielsrbezerra@gmail.com>
2022-03-17 01:44:13 -0300

    debugging frontend upload

--- udagram-frontend/src/app/api/api.service.ts
+++ udagram-frontend/src/app/api/api.service.ts
@@ -55,15 +55,16 @@ export class ApiService {
   }
 
   async upload(endpoint: string, file: File, payload: any): Promise<any> {
+      console.log("got to upload")
     const signed_url = (await this.get(`${endpoint}/signed-url/${file.name}`)).url;
-
+    console.log('signed url', signed_url);
     const headers = new HttpHeaders({'Content-Type': file.type});
     const req = new HttpRequest( 'PUT', signed_url, file,
                                   {
                                     headers: headers,
                                     reportProgress: true, // track progress
                                   });
-
+    console.log(req)
     return new Promise ( resolve => {
         this.http.request(req).subscribe((resp) => {
         if (resp && (<any> resp).status && (<any> resp).status === 200) {
