b5022947f1fba8794222622dcbddd0d3b04b3ae4
thegux <gabrielsrbezerra@gmail.com>
2022-03-19 09:42:17 -0300

    test commit

--- udagram-frontend/src/app/app.component.ts
+++ udagram-frontend/src/app/app.component.ts
@@ -8,7 +8,7 @@ import { environment } from '../environments/environment';
 @Component({
   selector: 'app-root',
   templateUrl: 'app.component.html'
-})
+})   
 export class AppComponent {
   public appPages = [
     {
